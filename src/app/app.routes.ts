f6caf69fda1d8c46e787b967bcc86118b2ee66e2
UT011627 <UT011627@UNICOMTIC71>
2026-09-12 15:50:39 +0530

    feat(gym): integrate gym routes and http configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'gym',
+    loadChildren: () =>
+      import('./features/gym/gym.routes')
+        .then(m => m.GYM_ROUTES)
+  }
+];
